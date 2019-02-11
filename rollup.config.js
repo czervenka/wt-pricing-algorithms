05f5bdad46c16aa1217a700d99bf6606da082b6e
Jirka Chadima <chadima.jiri@gmail.com>
2019-02-11 16:01:36 +0100

    build: Webpack -> Rollup

---
+++ rollup.config.js
@@ -0,0 +1,33 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import { terser } from 'rollup-plugin-terser';
+import pkg from './package.json';
+
+export default [
+  {
+    input: 'src/index.js',
+    output: {
+      name: 'wtPricingAlgorithms',
+      file: pkg.browser,
+      format: 'umd',
+      sourcemap: true
+    },
+    plugins: [
+      resolve(),
+      commonjs(),
+      terser(),
+    ],
+  },
+  {
+    input: 'src/index.js',
+    external: ['dayjs', 'currency.js'],
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' },
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+    ],
+  },
+];
